543a04fcc5afc3577c2dc57e5953ee7290f191b8
sonu kumar <krsonu002@gmail.com>
2025-04-07 11:07:19 +0530

    day 2

--- basicsproblem.cpp
+++ basicsproblem.cpp
@@ -1,16 +1,30 @@
 // 1.	WAP to check A character is a vowel or consonant
-#include<iostream>
-using namespace std;
-int main()
-{
-    cout << "enter a character";
-    char ch;
-    cin >> ch;
-    if (ch == 97 || ch == 101 || ch == 105 || ch == 111 || ch == 117 || ch == 65 || ch == 69 || ch == 73 || ch == 79 || ch == 55)
-    {
-        cout << "vowel";
-    }
-        else
-        cout << "consonent";
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     cout << "enter a character";
+//     char ch;
+//     cin >> ch;
+//     if (ch == 97 || ch == 101 || ch == 105 || ch == 111 || ch == 117 || ch == 65 || ch == 69 || ch == 73 || ch == 79 || ch == 55)
+//     {
+//         cout << "vowel";
+//     }
+//         else
+//         cout << "consonent";
 
+// }
+
+// 2 WAP to check A character is an alphabet or not
+#include <bits/stdc++.h> 
+int findCase(char ch) {
+    // Write your code here.
+    if(islower(ch)){
+        return 0;
+    }
+    if(isupper(ch)){
+        return 1;
+    }
+    else
+    return -1;
 }
