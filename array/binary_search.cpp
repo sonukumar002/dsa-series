aec5f9cd809f284bf224f2824314301fed173471
sonu kumar <krsonu002@gmail.com>
2025-04-19 00:01:04 +0530

    binary search

--- array/binary_search.cpp
+++ array/binary_search.cpp
@@ -1,5 +1,9 @@
 // [problem statement]:(https://www.naukri.com/code360/problems/sort-an-array-of-0s-1s-and-2s_892977)
 // solution
+#include<iostream>
+#include<vector>
+using namespace std;
+
 void sortArray(vector<int>& arr, int n){
     int count0=0;
     int count1=0;
@@ -26,4 +30,26 @@ void sortArray(vector<int>& arr, int n){
     for(int i=count0+count1;i<n;i++){
         arr[i]=2;
     }
+}
+
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter the elements (only 0, 1, or 2): ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    sortArray(arr, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
